7c628d30bd4904943c7769e4e26b70972aa32088
ibnuzaman <ibnuzaman2133@gmail.com>
2024-08-31 06:16:46 +0700

    Blade Templating

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:title>
+        {{ $title }}
+    </x-slot:title>
+    <h4>Author: {{ $nama }}</h4>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:title>
+        {{ $title }}
+    </x-slot:title>
+    <h3>Selamat Datang di Latihan Laravel 11</h3>
+</x-layout>
